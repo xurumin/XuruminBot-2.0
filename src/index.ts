3a62475ffbb396cb57c6515af0aeabdab57284f4
Jonatas A <jonatasaraujofx@gmail.com>
2022-07-22 14:10:20 -0300

    🎨 update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,34 @@
-import { Commands } from "./commands";
+import "dotenv/config";
+
+import {
+  APIApplicationCommandInteraction,
+  InteractionType,
+} from "discord-api-types/v10";
+
+import { Commands } from "./commands/commands";
+import * as Plugins from "./commands/Plugins";
+
+import { login } from "./config";
 
 (async () => {
-  await Commands();
+  const client = await login();
+
+  const plugins = await Plugins.loadPlugins();
+
+  const pluginsDict: any = await Plugins.registerPlugins(plugins);
+
+  client.on(
+    "interactionCreate",
+    async (interaction: APIApplicationCommandInteraction) => {
+      if (interaction.type !== InteractionType.ApplicationCommand) return;
+
+      const { commandName } = interaction as any;
+
+      if (pluginsDict[commandName]) {
+        return await pluginsDict[commandName](interaction);
+      }
+    }
+  );
+
+  // await Commands();
 })();
